9eee88ad1ecad2c173cfd4bdc11774ede9448e09
Eldiyar <eldiyarbakirov00@gmail.com>
2024-04-12 16:00:08 +0600

    edev

---
+++ src/pages/Page/MainPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Home from '../Home/Home'
+
+const MainPage: React.FC = () => {
+	return (
+		<div>
+			<Header />
+			<Home />
+		</div>
+	)
+}
+
+export default MainPage
